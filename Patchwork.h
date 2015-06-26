d55c1c32061615f253f113a6055c9c459bd6edfd
patricksnape <patricksnape@gmail.com>
2015-06-26 09:25:25 +0100

    Add fix for mingw FFTW compiled with MSVC
    See the comment for more details. This stops the segfault
    that was occuring when using the pre-compiled FFTW libraries.

--- Patchwork.h
+++ Patchwork.h
@@ -29,6 +29,23 @@
 
 #include <fftw3.h>
 
+#if defined(_WIN32) && defined(_MSC_VER)
+// This is required due to the writing of the wisdom file and the fact that
+// multiple C runtime libraries may be being used on Windows. This comes from
+// the fact we use the pre-compiled mingw FFTW DLLs, but compile this project
+// with MSVC. See http://www.fftw.org/install/windows.html for more
+// information.
+static void my_fftw_write_char(char c, void *f) { fputc(c, (FILE *) f); }
+#define fftw_export_wisdom_to_file(f) fftw_export_wisdom(my_fftw_write_char, (void*) (f))
+#define fftwf_export_wisdom_to_file(f) fftwf_export_wisdom(my_fftw_write_char, (void*) (f))
+#define fftwl_export_wisdom_to_file(f) fftwl_export_wisdom(my_fftw_write_char, (void*) (f))
+
+static int my_fftw_read_char(void *f) { return fgetc((FILE *) f); }
+#define fftw_import_wisdom_from_file(f) fftw_import_wisdom(my_fftw_read_char, (void*) (f))
+#define fftwf_import_wisdom_from_file(f) fftwf_import_wisdom(my_fftw_read_char, (void*) (f))
+#define fftwl_import_wisdom_from_file(f) fftwl_import_wisdom(my_fftw_read_char, (void*) (f))
+#endif
+
 namespace FFLD
 {
 /// The Patchwork class computes convolutions much faster than the HOGPyramid class.
